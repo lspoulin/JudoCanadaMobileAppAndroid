741301f97fb530291c4d2c5b443d417e456977ea
Unknown <lspoulin@gmail.com>
2018-05-07 10:39:41 -0400

    Successfully added brand icon

--- app/src/main/java/org/judocanada/judocanadamobileappandroid/SplashScreen.java
+++ app/src/main/java/org/judocanada/judocanadamobileappandroid/SplashScreen.java
@@ -1,6 +1,8 @@
 package org.judocanada.judocanadamobileappandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,18 +14,22 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run(){
+                finish();
             }
-        });
+        }, 3000);
+
+    }
+
+    @Override
+    public void finish(){
+        Intent result = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(result);
+        super.finish();
     }
 
 }
