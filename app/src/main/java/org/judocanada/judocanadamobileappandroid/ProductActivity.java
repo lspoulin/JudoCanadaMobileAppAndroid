e28cf8bc2a10bfb87c43267974914d5ea631a36b
Unknown <lspoulin@gmail.com>
2018-06-07 16:46:59 -0400

    Added the product_activity layout to the product_activity

--- app/src/main/java/org/judocanada/judocanadamobileappandroid/ProductActivity.java
+++ app/src/main/java/org/judocanada/judocanadamobileappandroid/ProductActivity.java
@@ -25,7 +25,7 @@ public class ProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
-        setContentView(R.layout.activity_post);
+        setContentView(R.layout.activity_product);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
